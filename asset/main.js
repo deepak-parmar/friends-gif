9fe605a9b6e55c8d8e288f1c32ff76c712558606
Deepak-Parmar <dgparmar1999@rediffmail.com>
2021-07-27 19:40:35 +0530

    Added assets and files into repo

---
+++ asset/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    url = new URL(window.location.href);
+    if (url.searchParams.get("image")) {
+        $.getJSON('asset/data.json', function(data) {
+            $("title").text(data[url.searchParams.get("image")].gif + " | GIFs from FRIENDS");
+            $("#episode-gif").prop("src", "asset/images/" + data[url.searchParams.get("image")].gif + ".gif");
+            $("#episode-number").text(data[url.searchParams.get("image")].episode.number);
+            $("#episode-title").text(data[url.searchParams.get("image")].episode.title);
+            
+            data[url.searchParams.get("image")].character.forEach(element => {
+                $("#episode-character").append("<li>"+ element +"</li>");
+            });
+
+            data[url.searchParams.get("image")].keyword.forEach(element => {
+                $("#episode-keyword").append("<li>"+ element +"</li>");
+            });
+
+            $("body").css("background", "url(asset/images/" + data[url.searchParams.get("image")].gif + ".gif)");
+            $("body").css("background-size", "cover");
+        });
+    } else { // if product is not selected page is redirected to home
+        //window.location.replace("/mcu-armoury/");
+        window.location.replace("/");
+    }
+});
